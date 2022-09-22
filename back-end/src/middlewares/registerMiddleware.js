d4fadbb8f695f1b1541ca7efd715ea19ec8ba5fb
Renato <renatoft89@gmail.com>
2022-09-22 15:50:57 -0300

    feat: implementa validRegister

---
+++ back-end/src/middlewares/registerMiddleware.js
@@ -0,0 +1,21 @@
+const Joi = require('joi');
+
+const NEW_USER = Joi.object({
+  name: Joi.string().min(12).required(),
+  email: Joi.string().min(3).required().email(),
+  password: Joi.string().min(6).required(),
+  role: Joi.required(),
+});
+
+const validRegister = async (req, res, next) => {
+  const { error } = NEW_USER.validate(req.body);
+  console.log(error);
+  if (error) {
+    next(error);
+  }
+  next();
+};
+
+module.exports = {
+  validRegister,
+};
