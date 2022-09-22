468717a493caf672c06b94b78ed0f546e0f24b8d
Renato <renatoft89@gmail.com>
2022-09-22 15:52:54 -0300

    feat: implementa funçao hash + verifica email name existe no db

--- back-end/src/controllers/register.controller.js
+++ back-end/src/controllers/register.controller.js
@@ -1,9 +1,16 @@
 const { createUser } = require('../services/register.services');
+const hashGenerator = require('../utils/hashGenerator');
 
 const createUserController = async (req, res, _next) => {
   const { name, email, password, role } = req.body;
 
-  await createUser({ name, email, password, role });
+  const hashPassword = hashGenerator(password);
+
+  const result = await createUser({ name, email, password: hashPassword, role });
+
+  if (!result) {
+    return res.status(409).json({ message: 'user or name exists in database' });
+  }
 
   return res.status(201).json({ message: 'ok' });
 };
--- back-end/src/services/register.services.js
+++ back-end/src/services/register.services.js
@@ -1,6 +1,11 @@
 const { User } = require('../database/models');
 
 const createUser = async ({ name, email, password, role }) => {
+  const emailExists = await User.findOne({ where: { email } });
+  const nameExists = await User.findOne({ where: { name } });
+
+  if (emailExists || nameExists) return false;
+  
   const newUser = await User.create({ name, email, password, role });
 
   return newUser;
