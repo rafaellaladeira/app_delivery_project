f2a3d5bef8482c68fa05ee07865ed9f2ba4f4b2a
patrik <pcr_batista@hotmail.com>
2022-09-23 18:31:38 -0300

    fix: corrige erros de relacionamento

--- back-end/src/database/models/salesproducts.model.js
+++ back-end/src/database/models/salesproducts.model.js
@@ -1,30 +1,68 @@
-const salesProductsSchema = (sequelize, DataTypes) => {
-  const salesProducts = sequelize.define("SalesProduct", {
-    saleId: { type: DataTypes.NUMBER, primaryKey: true },
-    productId: { type: DataTypes.NUMBER, primaryKey: true },
+// const salesProductsSchema = (sequelize, DataTypes) => {
+//   const salesProducts = sequelize.define("SalesProduct", {
+//     saleId: { type: DataTypes.NUMBER, primaryKey: true },
+//     productId: { type: DataTypes.NUMBER, primaryKey: true },
+//     quantity: DataTypes.NUMBER
+//   },
+//     {
+//       timestamps: false,
+//       tableName: 'salesProducts',
+//       underscored: true
+//     });
+
+//   return salesProducts;
+// };
+
+// salesProducts.associate = (models) => {
+//   models.Sale.belongsToMany(models.Product, {
+//     as: 'product',
+//     through: salesProducts,
+//     foreigKey: 'saleId',
+//     otherKey: 'productId'
+//   })
+
+//   models.Product.belongsToMany(models.Sale, {
+//     as: 'sale',
+//     through: salesProducts,
+//     foreigKey: 'productId',
+//     otherKey: 'saleId'
+//   })
+// }
+
+// module.exports = salesProductsSchema;
+
+
+module.exports = (sequelize, DataTypes) => {
+  const saleProductModel = sequelize.define('SalesProduct', {
+    saleId: { type: DataTypes.INTEGER, foreignKey: true, primaryKey: true },
+    productId: { type: DataTypes.INTEGER, foreignKey: true, primaryKey: true },
     quantity: DataTypes.NUMBER
   },
-    {
-      timestamps: false,
-    });
+  {
+    timestamps: false, 
+    tableName: 'salesProducts',
+    underscored: true
+  });
 
-  return salesProducts;
+  saleProductModel.associate = (models) => {
+    models.Sale.belongsToMany(models.Product,
+      { 
+        through: saleProductModel, 
+        as: 'product', 
+        foreignKey: 'saleId', 
+        otherKey: 'productId'
+      });
+
+      saleProductModel.associate = (models) => {
+        models.Product.belongsToMany(models.Sale,
+          { 
+            through: saleProductModel, 
+            as: 'product', 
+            foreignKey: 'productId', 
+            otherKey: 'saleId'
+          });
+      };
+  };  
+
+  return saleProductModel
 };
-
-salesProducts.associate = (models) => {
-  models.Sale.belongsToMany(models.Product, {
-    as: 'product',
-    through: salesProducts,
-    foreigKey: 'saleId',
-    otherKey: 'productId'
-  })
-
-  models.Product.belongsToMany(models.Sale, {
-    as: 'sale',
-    through: salesProducts,
-    foreigKey: 'productId',
-    otherKey: 'saleId'
-  })
-}
-
-module.exports = salesProductsSchema;
