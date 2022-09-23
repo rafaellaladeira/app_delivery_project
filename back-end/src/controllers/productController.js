434ad6ca89e7ee2469a585658da23630a95b3257
patrik <pcr_batista@hotmail.com>
2022-09-23 18:27:38 -0300

    feat: implementa products controller

---
+++ back-end/src/controllers/productController.js
@@ -0,0 +1,10 @@
+const productService = require('../services/productService')
+
+const getAllProducts = async(req, res, _next) => {
+  const products = await productService.getAllProducts();
+  return res.status(200).json(products);
+}
+
+module.exports = { 
+  getAllProducts,
+}
