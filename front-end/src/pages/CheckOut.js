a729128d847bf27561e77d12c3b357804a6a0a14
rafaellaladeira <rafaella.ladeira@gmail.com>
2022-09-25 10:56:51 -0300

    feat: adição de inputs e funções na rota customer/checkout

--- front-end/src/pages/CheckOut.js
+++ front-end/src/pages/CheckOut.js
@@ -1,14 +1,50 @@
 import React, { useEffect, useState } from 'react';
 import { getProductsCart } from '../services/localStorage';
+import { getInfo, request } from '../services/request';
 
 function Checkout() {
   const columnNames = ['Item', 'Descrição', 'Quantidade',
     'Valor unitário', 'Sub-total', 'Remover Item'];
+  const [address, setAddress] = useState('');
+  const [number, setNumber] = useState('');
+  const [sellerName, setSellerName] = useState([]);
+  const [seller, setSeller] = useState('');
   const [productsCart, setProductsCart] = useState([]);
   console.log(productsCart);
+  // const testId = 'customer_checkout__';
+
+  const handleChange = ({ target: { name, value } }) => {
+    if (name === 'address') setAddress(value);
+    if (name === 'number') setNumber(value);
+    if (name === 'seller') setSeller(value);
+  };
+
+  //  const handleClickRemove = () => {
+
+  //  };
+
+  const handleSubmit = async () => {
+    const data = {
+      sellerName: seller,
+      deliveryAddress: address,
+      deliveryNumber: number,
+    };
+    try {
+      const result = await request('customer/checkout', data);
+      return result;
+    } catch (error) {
+      return error;
+    }
+  };
+
+  const getNamesSeller = async () => {
+    const names = await getInfo('customer/checkout', 'seller');
+    setSellerName(names);
+  };
   useEffect(() => {
+    getNamesSeller();
     setProductsCart(getProductsCart());
-  });
+  }, []);
 
   return (
     <div>
@@ -24,8 +60,104 @@ function Checkout() {
             }
           </tr>
         </thead>
+
+        {/* {productsCart.map((item) => (
+
+          <tbody key={ item.name }>
+            <tr>
+              <td
+                data-testid={ `element-order-table-item-number-${item.item}` }
+              >
+                { item.item }
+
+              </td>
+              <td
+                data-testid={ `element-order-table-name-${item.item}` }
+              >
+                { item.name }
+
+              </td>
+              <td
+                data-testid={ `element-order-table-quantity-${item.item}` }
+              >
+                { item.quantity}
+
+              </td>
+              <td
+                data-testid={ `element-order-table-unit-price-${item.item}` }
+              >
+                { item.vunit }
+
+              </td>
+              <td
+                data-testid={ `element-order-table-sub-total-${item.item}` }
+              >
+                { item.total }
+              </td>
+              <td>
+                <button
+                  type="button"
+                  data-testid={ `element-order-table-remove-${item.item}` }
+                  onClick={ handleClick }
+                >
+                  Remover
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        ))} */}
       </table>
-      <p>loren</p>
+
+      <section>
+
+        <h2>Detalhes e Endereço para Entrega</h2>
+
+        { sellerName.map((name) => (
+          <label
+            key={ name }
+            htmlFor="seller"
+          >
+            P. Vendedora responsável
+
+            <select
+              name="seller"
+              onChange={ handleChange }
+              data-testid="customer_checkout__select-seller"
+            >
+              <option>{ name }</option>
+            </select>
+          </label>
+
+        ))}
+
+        <label htmlFor="address">
+          Endereço
+          <input
+            name="address"
+            onChange={ handleChange }
+            data-testid="customer_checkout__input-address"
+            placeholder="Rua Bom Sucesso"
+          />
+        </label>
+
+        <label htmlFor="number">
+          Número
+          <input
+            name="number"
+            onChange={ handleChange }
+            data-testid="customer_checkout__input-address-number"
+            placeholder="198"
+          />
+        </label>
+
+        <button
+          type="submit"
+          onClick={ handleSubmit }
+          data-testid="customer_checkout__button-submit-order"
+        >
+          Finalizar pedido
+        </button>
+      </section>
     </div>
   );
 }
