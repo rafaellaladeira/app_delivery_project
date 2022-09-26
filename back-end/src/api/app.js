4b8946361d25c5e1d5f69a16b9f1aa5c8c70f66d
Renato <renatoft89@gmail.com>
2022-09-26 13:58:10 -0300

    feat: adiciona express static

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,12 +4,16 @@ const errorMiddleware = require('../middlewares/error.middleware');
 const loginRouter = require('../routers/login.router');
 const registerRouter = require('../routers/register.router');
 const customerRouter = require('../routers/product.router');
+const path = require('path');
 require('express-async-errors');
 
 const app = express();
 
 app.use(express.json());
+
 app.use(cors());
+// http://expressjs.com/en/starter/static-files.html
+app.use('/images', express.static('public/images'))
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRouter);
