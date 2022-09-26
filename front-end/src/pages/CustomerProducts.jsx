c542a47ca3ac66e7c043913277ed5d729dfd12d2
Renato <renatoft89@gmail.com>
2022-09-26 09:23:04 -0300

    feat: criado page customer products

---
+++ front-end/src/pages/CustomerProducts.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import CardProduct from '../components/CustomerProducts/CardProduct';
+import NavProducts from '../components/CustomerProducts/NavProducts';
+
+function CustomerProducts() {
+  return (
+    <>
+      <NavProducts />
+      <CardProduct />
+    </>
+  );
+}
+
+export default CustomerProducts;
