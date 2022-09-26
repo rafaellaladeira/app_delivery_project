a41137105d94262c2798cdf49a1eeaed11528cee
Renato <renatoft89@gmail.com>
2022-09-26 14:00:52 -0300

    feat: cria a pagina CustomerProduct

--- front-end/src/pages/CustomerProducts.jsx
+++ front-end/src/pages/CustomerProducts.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import CardProduct from '../components/CustomerProducts/CardProduct';
 import NavProducts from '../components/CustomerProducts/NavProducts';
+import { getAllProducts } from '../services/request';
 
 function CustomerProducts() {
+  const [listProducts, setListProducts] = useState([]);
+
+  const getAll = useCallback(async () => {
+    const products = await getAllProducts('customer/products');
+    console.log(products);
+    setListProducts(products);
+  });
+
+  useEffect(() => {
+    getAll();
+  }, []);
+
   return (
     <>
       <NavProducts />
-      <CardProduct />
+      {
+        listProducts.map((product) => (
+          <CardProduct
+            key={ product.id }
+            name={ product.name }
+            price={ product.price }
+            urlImage={ product.urlImage }
+          />
+        ))
+      }
     </>
   );
 }
