88ee4f5250e6869586a99ea296bc860a9da9afd1
Renato <renatoft89@gmail.com>
2022-09-26 09:24:45 -0300

    feat: cria o compoente CardProduct

---
+++ front-end/src/components/CustomerProducts/CardProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CardProduct() {
+  return (
+    <section className="card-products">
+      <h1>Cards</h1>
+    </section>
+  );
+}
+
+export default CardProduct;
