325e8e52d3804de5632bdbefa82fecb32b6a4e1a
Renato <renatoft89@gmail.com>
2022-09-27 11:17:12 -0300

    fix: data-testid input quantity

--- front-end/src/components/CustomerProducts/CardProduct.jsx
+++ front-end/src/components/CustomerProducts/CardProduct.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function CardProduct({ id, name, price, urlImage }) {
+  // const [quantityProducts, setQuantityProducts] = useState(0);
   return (
     <section>
-      <span
+      <p
         data-testid={ `customer_products__element-card-price-${id}` }
       >
-        { price }
-      </span>
+        { new Intl.NumberFormat(
+          'pt-BR',
+          { style: 'currency', currency: 'BRL' },
+        ).format(price) }
+      </p>
       <img
+        width="200px"
         data-testid={ `customer_products__img-card-bg-image-${id}` }
         src={ urlImage }
         alt={ name }
@@ -22,15 +27,19 @@ function CardProduct({ id, name, price, urlImage }) {
       <button
         type="button"
         data-testid={ `customer_products__button-card-rm-item-${id}` }
-        // onClick={ valueQuantity(true) }
+        // onClick={ valueQuantity(false) }
       >
         -
       </button>
-      <span>0</span>
+      <input
+        type="text"
+        data-testid={ `customer_products__input-card-quantity-${id}` }
+        value={ quantityProducts }
+      />
       <button
         type="button"
         data-testid={ `customer_products__button-card-add-item-${id}` }
-        // onClick={ valueQuantity(false) }
+        // onClick={ valueQuantity(true) }
       >
         +
       </button>
