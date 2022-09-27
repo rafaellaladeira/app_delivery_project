b7e71d0e742f668395ce3da649378048aa3b8e89
Renato <renatoft89@gmail.com>
2022-09-27 12:41:09 -0300

    fix: import useMemo from react

--- front-end/src/components/Provider.js
+++ front-end/src/components/Provider.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
 import { getInfo } from '../services/request';
 import MyContext from '../context/MyContext';
