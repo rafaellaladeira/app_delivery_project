de349d727c7cbe76229fa11a546330444dca6847
Renato <renatoft89@gmail.com>
2022-09-27 12:22:32 -0300

    feat: adiciona funçao btn + ou - quantidade produto

--- front-end/src/components/CustomerProducts/CardProduct.jsx
+++ front-end/src/components/CustomerProducts/CardProduct.jsx
@@ -1,48 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import '../../styles/CustomerProducts.css';
 
 function CardProduct({ id, name, price, urlImage }) {
-  // const [quantityProducts, setQuantityProducts] = useState(0);
+  const [qtdProducts, setQtdProducts] = useState(0);
+
+  const valueQuantity = (verifyQtd) => (
+    verifyQtd ? setQtdProducts(qtdProducts + 1) : setQtdProducts(qtdProducts - 1));
+
   return (
-    <section>
-      <p
-        data-testid={ `customer_products__element-card-price-${id}` }
+    <section className="card-product">
+      <div
+        className="card-div-product"
       >
-        { new Intl.NumberFormat(
-          'pt-BR',
-          { style: 'currency', currency: 'BRL' },
-        ).format(price) }
-      </p>
-      <img
-        width="200px"
-        data-testid={ `customer_products__img-card-bg-image-${id}` }
-        src={ urlImage }
-        alt={ name }
-      />
-      <p
-        data-testid={ `customer_products__element-card-title-${id}` }
-      >
-        { name }
-      </p>
-      <button
-        type="button"
-        data-testid={ `customer_products__button-card-rm-item-${id}` }
-        // onClick={ valueQuantity(false) }
-      >
-        -
-      </button>
-      <input
-        type="text"
-        data-testid={ `customer_products__input-card-quantity-${id}` }
-        value={ quantityProducts }
-      />
-      <button
-        type="button"
-        data-testid={ `customer_products__button-card-add-item-${id}` }
-        // onClick={ valueQuantity(true) }
-      >
-        +
-      </button>
+        <p
+          data-testid={ `customer_products__element-card-price-${id}` }
+        >
+          { new Intl.NumberFormat(
+            'pt-BR',
+            { style: 'currency', currency: 'BRL' },
+          ).format(price) }
+        </p>
+        <img
+          width="200px"
+          data-testid={ `customer_products__img-card-bg-image-${id}` }
+          src={ urlImage }
+          alt={ name }
+        />
+        <p
+          data-testid={ `customer_products__element-card-title-${id}` }
+        >
+          { name }
+        </p>
+        <button
+          type="button"
+          data-testid={ `customer_products__button-card-rm-item-${id}` }
+          onClick={ () => valueQuantity(false) }
+          disabled={ qtdProducts < 1 }
+        >
+          -
+        </button>
+        <input
+          type="text"
+          data-testid={ `customer_products__input-card-quantity-${id}` }
+          value={ qtdProducts }
+          readOnly
+
+        />
+        <button
+          type="button"
+          data-testid={ `customer_products__button-card-add-item-${id}` }
+          onClick={ () => valueQuantity(true) }
+        >
+          +
+        </button>
+      </div>
     </section>
   );
 }
